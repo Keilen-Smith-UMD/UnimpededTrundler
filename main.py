1a2f84b6a01973906c5cd747b0d40b60d2776ec3
Keilen-Smith-UMD <smi02720@d.umn.edu>
2024-09-11 17:40:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,10 @@ import matplotlib.pyplot as pyplot
 print("Retrieving dataset...")
 adult = fetch_ucirepo(id=2) 
 print("Dataset retrieved.")
-  
+
 # data (as pandas dataframes) 
 X = adult.data.features 
 y = adult.data.targets 
-  
-# metadata 
-#print(adult.metadata) 
 
 # variable information, also available on the webpage: https://archive.ics.uci.edu/dataset/2/adult
 print(adult.variables) 
@@ -26,4 +23,19 @@ age = pandas.DataFrame(X.loc[:,"age"])
 hpw = pandas.DataFrame(X.loc[:,"hours-per-week"])
 age.insert(1, "hours-per-week", hpw)
 age.plot.scatter(x="age",y="hours-per-week")
+#pyplot.show()
+
+
+#data cleaning example: remove those who work less than full time or are unemployed, and limit our search to the U.S.
+df = pandas.DataFrame(X)
+df.dropna()
+#remove the unemployed
+df = df.drop(df[df['workclass'] == 'Never-worked'].index)
+#purge the sloths
+df = df.drop(df[df['hours-per-week'] >= 40].index)
+#enforce the borders
+selection = df['native-country'] == 'United-States'
+df = df[selection]
+subselection = df[['age', 'hours-per-week']]
+subselection.plot.scatter(x="age",y="hours-per-week")
 pyplot.show()
